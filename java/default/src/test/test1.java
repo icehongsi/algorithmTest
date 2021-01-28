8573ceca7191dfb960d748fb83b0354d4c5ea972
icehongsi <52072412+icehongsi@users.noreply.github.com>
2021-01-28 23:23:40 +0900

    .

---
+++ java/default/src/test/test1.java
@@ -0,0 +1,19 @@
+package test;
+
+public class test1 {
+	
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i ++) {
+			System.out.println("hello!");
+		}
+			
+		int num = 0;
+		
+		while (num < 5) {
+			System.out.println("bye!");
+			num ++;
+		}
+		
+	}
+
+}
